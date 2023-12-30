bb0e596701bc63d9055242884bf6070c4c6d06bd
Saavaj Studios <saavajgroup@192.168.1.4>
2023-12-30 09:53:05 +0530

    UI APP Created

---
+++ PetsUI/PetsUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetsUI
+//
+//  Created by Saavaj Studios on 30/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetsUI/PetsUI/PetsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsUIApp.swift
+//  PetsUI
+//
+//  Created by Saavaj Studios on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PetsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
